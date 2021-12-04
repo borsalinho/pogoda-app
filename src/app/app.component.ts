103135b0e3ed0dbe283984d9c73951ccdc01aa1f
borsalinho <dfvgbhcvbn0@gmail.com>
2021-12-04 12:16:20 +0900

    привязка API и вывод на один город

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pogoda-app';
+  cityName : string ="";
+  response: any;
+  
+
+  constructor(private http:HttpClient){
+
+  }
+  search(){
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?q='+this.cityName+'&appid=26312246dcd57cf04728a93f70af1fe8')
+    .subscribe((response)=>{
+      this.response = response;
+      console.log(this.response)
+    })
+  }
 }

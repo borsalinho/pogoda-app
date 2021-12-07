cf401b11fd0d391d917ec50c9dd836bc8ef4c998
borsalinho <dfvgbhcvbn0@gmail.com>
2021-12-07 16:00:32 +0900

    обработчик ошибки

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,23 @@ export class AppComponent {
   cityName : string ="";
   response: any;
   dataSource:[] | any = []; 
-
+  errorMessage:any;
   constructor(private http:HttpClient){
 
   }
   search(){
     this.http.get('https://api.openweathermap.org/data/2.5/weather?q='+this.cityName+'&appid=26312246dcd57cf04728a93f70af1fe8')
-    .subscribe((response)=>{
-      this.response = response;
-      console.log(this.response);
-      this.dataSource.push(this.response);
-      console.log(this.dataSource);
-    })
+    .subscribe(
+      (response)=>{
+        this.response = response;
+        console.log(this.response);
+        this.dataSource.push(this.response);
+        console.log(this.dataSource);
+      },
+      (error) => {
+        console.log(error);
+        this.errorMessage = "Город не найден"
+      },
+    )
   }
 }
